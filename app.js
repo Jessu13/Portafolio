fb5557d98f485d80fa083878e8fc4e08a24c716b
Jessuar Hoyos <jessuarhandan@gmail.com>
2022-07-08 10:06:42 -0500

    "Se agrega theme-btn"

--- app.js
+++ app.js
@@ -32,6 +32,14 @@ function pageTransition (){
             element.classList.add('active')
         }
     })
+
+    //Toggle theme
+
+    const themeBtn= document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 pageTransition();
